18ebc9c95248b6e15cd3f29520bed15a9a067e09
Rofayda <rofaydabassem@gmail.com>
2024-06-28 12:58:05 +0300

    feat: add blank kml model to be sent when clearing kmls

---
+++ lib/models/kml/blank_kml_model.dart
@@ -0,0 +1,24 @@
+class BlankKmlModel {
+  
+  String id;
+
+  BlankKmlModel({required this.id});
+
+  String get body => '''
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document id="$id">
+  </Document>
+</kml>
+  ''';
+
+  static String generateBlankKml(String id) {
+    return '''
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document id="$id">
+  </Document>
+</kml>
+    ''';
+  }
+}
